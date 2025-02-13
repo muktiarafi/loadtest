80c87db000b4fdf4e9a842b3674d9eb4f9a7b0b8
muktiarafi <muktiarafi@gmail.com>
2025-02-13 12:08:08 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	vegeta "github.com/tsenart/vegeta/v12/lib"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+)
+
+type Order struct {
+	Products    []*Product `json:"products"`
+	PaymentType string     `json:"paymentType"`
+}
+
+type Product struct {
+	ID       string `json:"id"`
+	Quantity int    `json:"quantity"`
+}
+
+func main() {
+	users, err := readFile("./users.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	producst, err := readFile("./products.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rate := vegeta.Rate{Freq: 3000, Per: time.Second}
+	duration := 10 * time.Minute
+
+	targeter := func() vegeta.Targeter {
+		return func(target *vegeta.Target) error {
+			target.Method = http.MethodPost
+			target.URL = "http://35.240.243.150/api/orders"
+			target.Header = http.Header{
+				"Content-Type":  []string{"application/json"},
+				"Authorization": {"Basic " + basicAuth(users[rand.Intn(len(users))], "")},
+			}
+
+			body1 := Order{
+				Products: []*Product{
+					{
+						ID:       producst[rand.Intn(len(producst))],
+						Quantity: 2,
+					},
+				},
+				PaymentType: "PAYMENT_TYPE_BALANCE",
+			}
+
+			target.Body, err = json.Marshal(body1)
+
+			return err
+		}
+	}()
+	attacker := vegeta.NewAttacker()
+
+	var metrics vegeta.Metrics
+	for res := range attacker.Attack(targeter, rate, duration, "Big Bang!") {
+		metrics.Add(res)
+	}
+	metrics.Close()
+
+	fmt.Printf("99th percentile: %s\n", metrics.Latencies.P99)
+}
+
+func readFile(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var users []string
+	for scanner.Scan() {
+		users = append(users, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
